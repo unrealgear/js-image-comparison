1e3f1d4ba8d80fd02e6bce089dbea7a250042f91
Nikita Riabchenko <unrealgear@gmail.com>
2019-02-12 23:07:29 +0200

    Added reading and generating of paths to GitHub and Google images.

--- src/paths.js
+++ src/paths.js
@@ -1,17 +1,43 @@
+const fileHelper = require('./helpers/fileHelper.js');
+
 const pathToImages = "./images/";
-const pathToExampleImages = pathToImages + "from_examples/";
 
-exports.pathToResults = "./results/";
-
-exports.paths = [
-    [pathToExampleImages + "opencv4nodejs/windows_actual.png", pathToExampleImages + "opencv4nodejs/windows_expected.png",
+var imagesToCompare = [
+    [pathToImages + "opencv4nodejs/windows_actual.png", pathToImages + "opencv4nodejs/windows_expected.png",
         "-windows.png"],
-    [pathToExampleImages + "pixelmatch/map1_actual.png", pathToExampleImages + "pixelmatch/map1_expected.png",
+    [pathToImages + "pixelmatch/map1_actual.png", pathToImages + "pixelmatch/map1_expected.png",
         "-map1.png"],
-    [pathToExampleImages + "pixelmatch/map2_actual.png", pathToExampleImages + "pixelmatch/map2_expected.png",
+    [pathToImages + "pixelmatch/map2_actual.png", pathToImages + "pixelmatch/map2_expected.png",
         "-map2.png"],
-    [pathToExampleImages + "pixelmatch/pins_actual.png", pathToExampleImages + "pixelmatch/pins_expected.png",
+    [pathToImages + "pixelmatch/pins_actual.png", pathToImages + "pixelmatch/pins_expected.png",
         "-pins.png"],
-    [pathToExampleImages + "resemblejs/people_actual.jpg", pathToExampleImages + "resemblejs/people_expected.jpg",
+    [pathToImages + "resemblejs/people_actual.jpg", pathToImages + "resemblejs/people_expected.jpg",
         "-people.png"]
 ];
+
+generateAndAddPathToImagesFrom(pathToImages + "github/", pathToImages + "baseline/github.png");
+generateAndAddPathToImagesFrom(pathToImages + "google/", pathToImages + "baseline/google.png");
+
+function generateAndAddPathToImagesFrom(pathToDir, pathToBaseLine, arrayToAddPathsTo = imagesToCompare) {
+
+    // read files from given dir, return array of strings, where string - is a paths to file
+    let filesInDirectory = fileHelper.getFilesInDir(pathToDir);
+
+    // initialize new empty array to write paths to actual, expected image and the name for the image
+    let arrayOfPathsToImages = [];
+
+    // generate and pushes the new array where [0] is a path to actual image, [1] is a path to expected image,
+    // [2] is a name of the file
+    filesInDirectory.forEach(function (pathToImage) {
+        arrayOfPathsToImages.push([pathToImage, pathToBaseLine,
+            "-" + pathToImage.substring(pathToImage.lastIndexOf("/") + 1)])
+    });
+
+    // pushes the new array with paths to the predefined one
+    arrayOfPathsToImages.forEach(function (arrayEntry) {
+        arrayToAddPathsTo.push(arrayEntry)
+    })
+}
+
+exports.pathToResults = "./results/";
+exports.imagesToCompare = imagesToCompare;
